1d91dcb60f5e5ea4ad6df125ce29f5afab427583
mathiax63 <mathyoyo@hotmail.es>
2021-10-06 17:56:02 -0300

    trabajo UTN

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const port = 3030
+const path = require("path")
+app.use(express.static("public"))
+const port = process.env.PORT;
+
+app.listen(port || 3000, () => {
+    if (port == undefined){
+        console.log('Servidor corriendo en el puerto ' + 3000)
+    } else {
+        console.log('Servidor corriendo en el puerto ' + port)
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+});
